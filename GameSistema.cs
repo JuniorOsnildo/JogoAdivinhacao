82077136fdd29b44924ea42a4ca9ddf0315a0df3
Osnildo <juniorosnildo777@gmail.com>
2025-03-20 19:54:10 -0300

    1

---
+++ GameSistema.cs
@@ -0,0 +1,12 @@
+﻿namespace JogoAdivinhação;
+
+public class GameSistema
+{
+    public int GameStart()
+    {
+        Random random = new Random();
+        int resposta = random.Next(1, 100);
+        
+        return resposta;
+    }
+}
