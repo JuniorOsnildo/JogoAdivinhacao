c58a3de8b893e8ae1ffe95e999460607ea6c2e0d
Osnildo <juniorosnildo777@gmail.com>
2025-03-20 21:07:05 -0300

    pronto

--- GameSistema.cs
+++ GameSistema.cs
@@ -2,11 +2,136 @@
 
 public class GameSistema
 {
-    public int GameStart()
+
+    private int _resposta;
+    private int _pontos;
+    private int _rodada = 1;
+    private int _rodadafinal;
+    private int _dificuldade;
+    
+    
+   /*
+    * Difculdades:
+    * 1 -> fácil
+    * 2 -> médio
+    * 3 -> difícil
+    */
+    
+    public void GameStart()
     {
-        Random random = new Random();
-        int resposta = random.Next(1, 100);
+        EscolherDificuldade();
         
-        return resposta;
+        Random random = new Random();
+        this._resposta = random.Next(1, this._dificuldade*12);
+        this._rodadafinal = 10 - this._dificuldade;
     }
+    
+    public int ReceberPalpite()
+    {
+        int palpite = 0;
+        
+        try
+        {
+           palpite = int.Parse(Console.ReadLine() ?? "1");
+        }
+        catch
+        {
+            ReceberPalpite();
+        }
+        
+        return palpite;
+    }
+
+    private void EscolherDificuldade()
+    {
+        Console.WriteLine("Escolha uma dificuldade");
+        Console.WriteLine("1 -> fácil");
+        Console.WriteLine("2 -> médio");
+        Console.WriteLine("3 -> difícil");
+        Console.Write("-> " );
+        int escolha = int.Parse(Console.ReadLine() ?? "1");
+        
+        this._dificuldade = escolha;
+    }
+
+
+    public Boolean VerificarResposta(int palpite)
+    {
+        Console.Clear();
+        Console.WriteLine("|--------------------------------------------------|");
+        Console.WriteLine("|--------------------------------------------------|\n");
+        
+        
+        
+        if (palpite > this._resposta)
+        {
+            Console.WriteLine("Respota incorreta! \nA resposta correta é menor");
+            Console.WriteLine("\n|--------------------------------------------------|");
+            Console.WriteLine("|--------------------------------------------------|");
+            Console.ReadKey();
+        }
+        else if (palpite < this._resposta)
+        {
+            Console.WriteLine("Respota incorreta! \nA resposta correta é maior");
+            
+            Console.WriteLine("\n|--------------------------------------------------|");
+            Console.WriteLine("|--------------------------------------------------|");
+            Console.ReadKey();
+        }
+        else if (palpite == this._resposta)
+        {
+            Console.WriteLine("REPOSTA CORRETA!");
+            CalcularPontos();
+            
+            Console.WriteLine("\n|--------------------------------------------------|");
+            Console.WriteLine("|--------------------------------------------------|");
+            
+            Console.ReadKey();
+            Vitoria();
+            return true;
+        }
+
+        if (this._rodada == this._rodadafinal)
+        {
+            Derrota();
+            return true;
+        }
+
+        this._rodada++;
+        return false;
+    }
+
+    private void CalcularPontos()
+    {
+        this._pontos = (this._rodadafinal+1 - this._rodada)*this._dificuldade*100;
+    }
+    
+    private void Vitoria()
+    {
+        Console.Clear();
+        Console.WriteLine("|--------------------------------------------------|");
+        Console.WriteLine("|--------------------------------------------------|\n\n");
+
+        Console.WriteLine("VOCÊ VENCEU!                   ");
+        Console.WriteLine("Pontuação: "+ this._pontos+"                   ");
+        
+        Console.WriteLine("\n\n|--------------------------------------------------|");
+        Console.WriteLine("|--------------------------------------------------|");
+        Console.ReadKey();
+    }
+
+    private void Derrota()
+    {
+        Console.Clear();
+        Console.WriteLine("|--------------------------------------------------|");
+        Console.WriteLine("|--------------------------------------------------|\n");
+
+        Console.WriteLine("Acabaram as tentativas");
+        Console.WriteLine(":(");
+        
+        Console.WriteLine("\n|--------------------------------------------------|");
+        Console.WriteLine("|--------------------------------------------------|");
+        Console.ReadKey();
+    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using JogoAdivinhação;
+
+
+GameSistema game = new GameSistema();
+    
+game.GameStart();
+
+Boolean rodar = true;
+
+while (rodar)
+{
+    Console.WriteLine("Digite seu palpite: ");
+
+    if (game.VerificarResposta(game.ReceberPalpite()))
+        rodar = false;
+    
+}
